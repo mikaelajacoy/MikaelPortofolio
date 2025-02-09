2e070492fd45460cf3889e0a426f8e059bfe899f
mikaelajacoy <165372572+mikaelajacoy@users.noreply.github.com>
2025-02-09 23:26:42 +0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Animasi smooth scroll untuk navigation bar
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
